ed6a9bccc7ed862f0b311f554bfe8407ffa6dfcf
JefClaes <jef@slotsgames.be>
2018-04-19 07:18:25 +0200

    Nicer output

--- build.bat
+++ build.bat
@@ -1,1 +1,1 @@
- pandoc -o "C:\book.epub" --columns 10000 --css epub.css meta.yaml  01.introduction.md 02.storage.md 03.distribution.md 04.importing.md 05.table-maintenance.md 06.exporting.md 07.query-processing.md 08.wlm.md 99.credits.md
+ pandoc -o  "C:\book.epub" --columns 10000 --number-sections --toc --css epub.css meta.yaml  01.introduction.md 02.storage.md 03.distribution.md 04.importing.md 05.table-maintenance.md 06.exporting.md 07.query-processing.md 08.wlm.md 99.credits.md
